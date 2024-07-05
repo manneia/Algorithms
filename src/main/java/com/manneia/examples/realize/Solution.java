a45f369d4e8908dd8d0cd1c02bf1f7d4de129eff
manneia <luokaixuan2001@163.com>
2024-07-05 10:11:08 +0800

    完成排序链表去重算法的编写

--- src/main/java/com/manneia/examples/realize/Solution.java
+++ src/main/java/com/manneia/examples/realize/Solution.java
@@ -553,4 +553,25 @@ public class Solution {
 		}
 		return b;
 	}
+
+	/**
+	 * 删除排序链表中的重复元素
+	 *
+	 * @param head 链表头结点
+	 * @return 返回去重后的链表头结点
+	 */
+	public ListNode deleteDuplicates(ListNode head) {
+		if (head == null) {
+			return head;
+		}
+		ListNode cur = head;
+		while (cur.next != null) {
+			if (cur.val == cur.next.val) {
+				cur.next = cur.next.next;
+			} else {
+				cur = cur.next;
+			}
+		}
+		return head;
+	}
 }
